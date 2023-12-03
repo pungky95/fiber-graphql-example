9a91ee44299a7081665edac4b5572303be941dac
Pungky Kurniawan <pungkykurniawankr@gmail.com>
2023-12-03 22:32:46 +0800

    🎨 code review

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,66 +15,53 @@ type Input struct {
 }
 
 func main() {
+	// Create GraphQL fields
 	fields := graphql.Fields{
 		"hello": &graphql.Field{
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
+				return "tokyo", nil
 			},
 		},
 	}
 
-	rootQuery := graphql.ObjectConfig{
-		Name:   "RootQuery",
-		Fields: fields,
-	}
+	// Create RootQuery object config
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
 
-	schemaConfig := graphql.SchemaConfig{
-		Query: graphql.NewObject(rootQuery),
-	}
+	// Create schema config with RootQuery
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
 
+	// Create schema
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
 	}
 
+	// Create Fiber app
 	app := fiber.New()
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		var input Input
-		if err := ctx.QueryParser(&input); err != nil {
-			return ctx.Status(fiber.StatusInternalServerError).
-				SendString("Cannot parse query parameters: " + err.Error())
-		}
-
-		result := graphql.Do(graphql.Params{
-			Schema:         schema,
-			RequestString:  input.Query,
-			OperationName:  input.OperationName,
-			VariableValues: input.Variables,
-		})
-
-		ctx.Set("Content-Type", "application/graphql-response+json")
-		return ctx.JSON(result)
-	})
-
-	app.Post("/", func(ctx *fiber.Ctx) error {
+	// Handler function for GraphQL requests
+	graphqlHandler := func(ctx *fiber.Ctx) error {
 		var input Input
 		if err := ctx.BodyParser(&input); err != nil {
-			return ctx.Status(fiber.StatusInternalServerError).
-				SendString("Cannot parse body: " + err.Error())
+			errMsg := fmt.Sprintf("Cannot parse request body: %v", err)
+			return ctx.Status(fiber.StatusInternalServerError).SendString(errMsg)
 		}
-
 		result := graphql.Do(graphql.Params{
 			Schema:         schema,
 			RequestString:  input.Query,
 			OperationName:  input.OperationName,
 			VariableValues: input.Variables,
 		})
-
 		ctx.Set("Content-Type", "application/graphql-response+json")
 		return ctx.JSON(result)
-	})
+	}
+
+	// Handle GET requests
+	app.Get("/", graphqlHandler)
+
+	// Handle POST requests
+	app.Post("/", graphqlHandler)
 
 	log.Fatal(app.Listen(":8080"))
 }
